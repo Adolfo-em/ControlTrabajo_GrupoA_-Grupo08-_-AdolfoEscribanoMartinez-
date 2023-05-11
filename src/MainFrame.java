631d902a8905cfd35dcf64db4d4a2b2cc2ac4f4a
adolfo-em <Adolfojose.escribano@alu.uclm>
2023-05-11 11:25:13 +0200

    Commit V1

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -30,6 +30,7 @@ public class MainFrame extends JFrame {
     private JTextField tfBuscarPais;
     private JButton calcularPaisButton;
     private JTextField tfResultadoPais;
+    private JButton btnBuscarPorPrio;
 
     private FileInputStream fis;
     private ObjectInputStream entrada;
@@ -98,6 +99,12 @@ public class MainFrame extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tfResultadoPais.setText(Integer.toString(puerto.contador(tfBuscarPais.getText())));
+            }
+        });
+        btnBuscarPorPrio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfMostrar_id.setText((puerto.buscaContenedoresBase(Integer.parseInt(tfPrioridad.getText()))));
             }
         });
     }
